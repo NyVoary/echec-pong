a0b1bbda413787c4e993c2226f7409da7589afe1
Harena-Git <faniriantsoaharena@gmail.com>
2025-12-15 09:26:28 +0300

    Mandeha

--- serveur.bat
+++ serveur.bat
@@ -4,7 +4,7 @@ REM Compile tout et démarre le serveur
 
 call compile.bat
 
-REM Lancer le serveur
-java -cp "echec-pong-server.jar;lib/wildfly-client-all-27.0.1.Final.jar" server.ServerMain
+REM Lancer le serveur avec les bibliothèques client WildFly 37
+java -cp "echec-pong-server.jar;D:\wildfly-37.0.1.Final\bin\client\jboss-client.jar" server.ServerMain
 
 pause
